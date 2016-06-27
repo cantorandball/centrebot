b5fcd160f4cac4bfd80e8022f3ed73cef3edb889
josh <joshsmith2@gmail.com>
2016-06-27 17:37:27 +0100

    Initial tests for parsing input to email questions

--- app/models/email_question.rb
+++ app/models/email_question.rb
@@ -1,2 +1,14 @@
+class InvalidEmailError < StandardError
+end
+
 class EmailQuestion < Question
+  def parse(incoming_text)
+    email_regex = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+    lower_text = incoming_text.downcase
+    error_message = "Sorry, that didn't seem to be a valid email address. "\
+                    "Could you try entering it again?"
+    if email_regex !~ lower_text
+      raise InvalidEmailError.new(error_message)
+    end
+  end
 end
--- spec/models/email_question_spec.rb
+++ spec/models/email_question_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe EmailQuestion do
   it "has a valid factory" do
     expect(create(:email_question)).to be_valid
   end
+
+  it "throws an exception when parsing addresses without an @" do
+    email_question = create(:email_question)
+
+    expect do
+      email_question.parse("whatevenisemail")
+    end.to raise_error(InvalidEmailError)
+  end
 end
