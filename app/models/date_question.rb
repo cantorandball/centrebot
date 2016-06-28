74f073464efac9566cb486bdcb1c5c7603998c52
josh <joshsmith2@gmail.com>
2016-06-28 23:30:16 +0100

    Tests for question's outcomes, and their values.
    Also removed some logic from DateQuestion which should not have been there

--- app/models/date_question.rb
+++ app/models/date_question.rb
@@ -1,10 +1,2 @@
 class DateQuestion < Question
-  def parse(incoming_text)
-    initial_parsed_text = super
-    if Phonelib.valid_for_country? initial_parsed_text, "GB"
-      initial_parsed_text
-    else
-      raise InvalidInputError.new()
-    end
-  end
 end
--- spec/models/multiple_choice_question_spec.rb
+++ spec/models/multiple_choice_question_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe MultipleChoiceQuestion do
   it "has a valid factory" do
     expect(create(:multiple_choice_question)).to be_valid
   end
+
+  it "has multiple outcomes with values" do
+    outcomes = create(:multiple_choice_question).outcomes
+    expect(outcomes.length).to eql(3)
+    outcomes.each do |outcome|
+      expect(outcome.value).to eql('3: pidgeons')
+    end
+  end
 end
