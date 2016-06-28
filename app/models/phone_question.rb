436dd6d0a2bf7bd6526962ec09c4f268e3750104
josh <joshsmith2@gmail.com>
2016-06-28 12:44:10 +0100

    Adds phonelib gem, includes inital parser code and tests for phone_question

--- app/models/phone_question.rb
+++ app/models/phone_question.rb
@@ -1,2 +1,10 @@
 class PhoneQuestion < Question
+  def parse(incoming_text)
+    initial_parsed_text = super
+    if Phonelib.valid_for_country? initial_parsed_text, "GB"
+      initial_parsed_text
+    else
+      raise InvalidInputError.new()
+    end
+  end
 end
