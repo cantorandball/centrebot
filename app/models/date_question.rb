d6bb1cd93a7505ca34054d8645def368395867b5
josh <joshsmith2@gmail.com>
2016-06-30 17:04:51 +0100

    Tests for date questions passing

--- app/models/date_question.rb
+++ app/models/date_question.rb
@@ -1,15 +1,12 @@
 class DateQuestion < Question
   def parse(incoming_text)
     incoming_parsed_text = super
-    date_format = '%d.%m.%Y'
-    dividers = '([:-|/\\\\ .])'
-    day_match = '(?<day>[0-9]{1,2})'
-    month_match = '(?<month>[0-9]{1,2})'
-    year_match = '(?<year>[0-9]{4})'
-    date_match = "#{day_match}#{dividers}#{month_match}#{dividers}#{year_match}"
-    matched_date = incoming_parsed_text.match(date_match)
-    if matched_date
-    else
+    out_format = '%d.%m.%Y'
+    begin
+      in_date = Date.parse(incoming_parsed_text)
+    rescue ArgumentError
+      raise(InvalidInputError)
     end
+    in_date.strftime(out_format)
   end
 end
--- spec/models/date_question_spec.rb
+++ spec/models/date_question_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe DateQuestion do
     dates_and_conversions = []
     dates_and_conversions.push({'in' => '9/8/1989', 'out' => '09.08.1989'})
     dates_and_conversions.push({'in' => '5.3.2001', 'out' => '05.03.2001'})
-    dates_and_conversions.push({'in' => '16\\11\\2003', 'out' => '16.11.2003'})
+    dates_and_conversions.push({'in' => '3rd Feb 1999', 'out' => '03.02.1999'})
+    dates_and_conversions.push({'in' => '21st July 1999', 'out' => '21.07.1999'})
     dates_and_conversions.each do |d_c|
       expect(date_question.parse(d_c['in'])).to eql(d_c['out'])
     end
@@ -18,11 +19,11 @@ RSpec.describe DateQuestion do
 
   it "does not parse invalid dates" do
     date_question = create(:date_question)
-    invalid_dates = %w(9.9.9 123402003 19.9.66 16^18^840.03.1090 18/29/2014)
+    invalid_dates = %w(123402003 19.9.66 0.03.1090 18/29/2014 5-23-1999)
     invalid_dates.each do |invalid_date|
       expect do
         date_question.parse(invalid_date)
-      end.to raise_error(InvalidInputError)
+      end.to raise_error(InvalidInputError), "#{invalid_date} should fail"
     end
   end
 end
