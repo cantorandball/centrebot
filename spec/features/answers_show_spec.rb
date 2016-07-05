2be731ff31dd3084f578efdf93a9380591c87752
josh <joshsmith2@gmail.com>
2016-07-05 14:50:13 +0100

    Failing tests for displaying existing outcomes on question page

--- spec/features/answers_show_spec.rb
+++ spec/features/answers_show_spec.rb
@@ -12,7 +12,12 @@ describe "View answers for a question", type: :feature do
   end
 
   it "shows the outcome dropdown populated with the next question" do
-    expect(page).to have_css(".next-question", text: @next_question.text)
-    expect(false)
+    next_q_xpath = "//option[text()=\"#{@next_question.text}\"]"
+    expect(page).to have_xpath(next_q_xpath, visible: true)
+  end
+
+  it "doesn't show the default message" do
+    default_xpath = '//option[text()="Select a question"]'
+    expect(page).not_to have_xpath(default_xpath, visible: true)
   end
 end
