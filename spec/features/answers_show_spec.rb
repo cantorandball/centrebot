bfd294978070a406461a69d2fb3b27bd422b2008
josh <joshsmith2@gmail.com>
2016-07-05 12:14:49 +0100

    Checking to see if rspec is broken on master too...

--- spec/features/answers_show_spec.rb
+++ spec/features/answers_show_spec.rb
@@ -3,9 +3,16 @@ require "spec_helper"
 describe "View answers for a question", type: :feature do
   before(:each) do
     @question = create(:question, text: "Do you have an answer to this?")
-    @outcome = create(:outcome, value: 'Oh yes')
+    @next_question = create(:question, text: "Is this the end?")
+    @outcome = create(:outcome, value: 'Oh yes',
+                      question: @question, next_question: @next_question)
     visit "/questions"
     button = find(:css, "#edit-question-" + @question.id.to_s)
     button.click
+  end
+
+  it "shows the outcome dropdown populated with the next question" do
+    expect(page).to have_css(".next-question", text: @next_question.text)
+    expect(false)
   end
 end
