a492d1f4c71b66547de7c6fd69a8ca94ca93787e
josh <joshsmith2@gmail.com>
2016-07-07 16:18:59 +0100

    it doesn't show the default message test now correctly set up and passing

--- spec/features/answers_show_spec.rb
+++ spec/features/answers_show_spec.rb
@@ -17,7 +17,8 @@ describe "View answers for a question", type: :feature do
   end
 
   it "doesn't show the default message" do
-    default_xpath = '//option[text()="Select a question"]'
-    expect(page).not_to have_xpath(default_xpath, visible: true)
+    first_dropdown_xpath = "//select[@name='outcome[next_question_id]']"
+    first_dropdown = first(:xpath, first_dropdown_xpath)
+    expect(first_dropdown.text).not_to eql("Select a question")
   end
 end
