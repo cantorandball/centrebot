6b40b57922c61741eb4ebf963181c746f11fb90c
josh <joshsmith2@gmail.com>
2016-07-12 14:56:28 +0100

    Fixed tests to reflect new answer setup

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,12 +1,17 @@
 require "rails_helper"
 
 RSpec.describe Answer do
+
   it "has a valid factory" do
     expect(create(:answer)).to be_valid
   end
 
   it "is valid without text" do
     expect(create(:answer, text: nil)).to be_valid
+  end
+
+  it "is not valid without question_text" do
+    expect(build(:answer, question_text: nil)).not_to be_valid
   end
 
   it "has a question" do
