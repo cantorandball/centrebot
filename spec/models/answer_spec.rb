6da2f400fd6b571dbd643e6839cc241f9020dd4c
josh <joshsmith2@gmail.com>
2016-07-12 15:45:52 +0100

    Wrote test for changing question texts, which passed first time

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -25,4 +25,21 @@ RSpec.describe Answer do
 
     expect(answer.responder).to be_a(Responder)
   end
+
+  it "records changes in question text" do
+    responder = create(:responder)
+    first_text = "Where do you live?"
+    second_text = "Where DON'T you live?"
+
+    changing_question = create(:question, text: first_text)
+    first_answer = changing_question.answer(responder, "Bristol")
+
+    changing_question.update_attribute(:text, second_text)
+    second_answer = changing_question.answer(responder, "Not Bristol")
+
+    expect(responder.answers.length).to eq(2)
+    expect(first_answer.question).to eq(second_answer.question)
+    expect(first_answer.question_text).to eq(first_text)
+    expect(second_answer.question_text).to eq(second_text)
+  end
 end
