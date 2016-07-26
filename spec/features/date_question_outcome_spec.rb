b953e32fef3f3d3a15b08efb87c35c3cd75f69d2
josh <joshsmith2@gmail.com>
2016-07-26 10:12:05 +0100

    Test for saving bound values passing

--- spec/features/date_question_outcome_spec.rb
+++ spec/features/date_question_outcome_spec.rb
@@ -15,4 +15,17 @@ describe "Add outcome to date question", type: :feature do
   it "displays options for date answers" do
     expect(page).to have_text("Earliest date")
   end
+
+  it "saves input values for lower and upper bounds" do
+    lower_id = "question_outcomes_attributes_0_lower_bound"
+    upper_id = "question_outcomes_attributes_0_upper_bound"
+
+    fill_in lower_id, with: 6
+    fill_in upper_id, with: 3
+
+    click_on "Add answer"
+
+    expect(@date_question.outcomes.first.lower_bound).to eq(6)
+    expect(@date_question.outcomes.first.upper_bound).to eq(3)
+  end
 end
